f3ba0b3f7d0fc728783cde8c99eb37641c35377d
random-entity <yakpanteki@gmail.com>
2021-06-05 14:43:50 +0900

    liquid sim tut following

---
+++ Human Forest Minimal/Assets/Scripts/GUI/PersonGUI.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PersonGUI : MonoBehaviour
+{
+    public Image EmotionBar;
+    [SerializeField] private Person person;
+
+    // private void UpdateEmotionBar()
+    // {
+    //     EmotionBar.fillAmount = person.Emotion;
+    // }
+
+    // void Update()
+    // {
+    //     UpdateEmotionBar();
+    // }
+}
