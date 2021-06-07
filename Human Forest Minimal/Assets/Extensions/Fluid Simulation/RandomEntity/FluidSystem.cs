4722cf5da108ebd312c5399653b26d2398335a60
random-entity <yakpanteki@gmail.com>
2021-06-07 11:00:40 +0900

    후하후하

--- Human Forest Minimal/Assets/Extensions/Fluid Simulation/RandomEntity/FluidSystem.cs
+++ Human Forest Minimal/Assets/Extensions/Fluid Simulation/RandomEntity/FluidSystem.cs
@@ -5,7 +5,7 @@ public class FluidSystem : MonoBehaviour
 {
     public List<CFloat2> SVList; // (x = State, y = Value) Pair
     private float count; // Runtime 동안 이 값이 바뀔 일은 없겠지.
-    [SerializeField] private List<(float, float, float)> XWHList; // (x = 왼쪽아래꼭지점의 x좌표) 이건 [0, 1]^3 기준.
+    [SerializeField] private List<(float, float, float, float)> XYWHList; // (xy = 왼쪽아래꼭지점의 x좌표) 이건 [0, 1]^3 기준.
     [SerializeField] private Vector3 FrameBottomLeftPosition, FrameWidthHeight;
     private List<Transform> RectList;
     [SerializeField] private Transform RectPrefab;
@@ -15,7 +15,7 @@ public class FluidSystem : MonoBehaviour
 
     private void Awake()
     {
-        particleDiameter = FluidParticlePrefab.GetComponent<CircleCollider2D>().radius * 2f;
+        particleDiameter = FluidParticlePrefab.transform.localScale.x * FluidParticlePrefab.GetComponent<CircleCollider2D>().radius * 2f;
         particleDiameterNormalizedX = particleDiameter / FrameWidthHeight.x;
         particleDiameterNormalizedY = particleDiameter / FrameWidthHeight.y;
 
@@ -32,10 +32,10 @@ public class FluidSystem : MonoBehaviour
             RectList.Add(rect_i);
         }
 
-        XWHList = new List<(float, float, float)>();
+        XYWHList = new List<(float, float, float, float)>();
 
         NormalizeValues();
-        UpdateXWHList();
+        UpdateXYWHList();
 
         RectPrefab.gameObject.SetActive(false);
 
@@ -46,31 +46,34 @@ public class FluidSystem : MonoBehaviour
     {
         for (int i = 0; i < count; i++)
         {
-            var xwh_i = XWHList[i];
-            float x = xwh_i.Item1;
-            float w = xwh_i.Item2;
-            float h = xwh_i.Item3;
+            var xywh_i = XYWHList[i];
+
+            float x = xywh_i.Item1;
+            float y = xywh_i.Item2;
+            float w = xywh_i.Item3;
+            float h = xywh_i.Item4;
 
             for (float px = x; px < x + w; px += particleDiameterNormalizedX)
             {
-                for (float py = 0f; py < h; py += particleDiameterNormalizedY)
+                for (float py = y; py < y + h; py += particleDiameterNormalizedY)
                 {
-                    ObjectPooler.SpawnFromPool("Fluid", XWH2Position((px, py, 0)));
+                    Debug.LogFormat("{0}, {1}, {2}, {3}", px, py, particleDiameterNormalizedX, particleDiameterNormalizedY);
+                    ObjectPooler.SpawnFromPool("Fluid", XYWH2Position((px, py, particleDiameterNormalizedX, particleDiameterNormalizedY)));
                 }
             }
         }
     }
 
-    private void UpdateXWHList()
+    private void UpdateXYWHList()
     {
-        XWHList.Clear();
+        XYWHList.Clear();
 
         float x = 0;
 
         for (int i = 0; i < count; i++)
         {
-            XWHList.Add((x, SVList[i].y, SVList[i].x));
-            MatchTransformToXWH(RectList[i], XWHList[i]);
+            XYWHList.Add((x, 0f, SVList[i].y, SVList[i].x));
+            MatchTransformToXYWH(RectList[i], XYWHList[i]);
             x += SVList[i].y;
         }
     }
@@ -112,20 +115,20 @@ public class FluidSystem : MonoBehaviour
     #endregion
 
     #region XWH2Vector3
-    private Vector3 XWH2Position((float x, float w, float h) xwh)
+    private Vector3 XYWH2Position((float x, float y, float w, float h) xywh)
     {
-        return FrameBottomLeftPosition + new Vector3(FrameWidthHeight.x * (xwh.x + 0.5f * xwh.w), FrameWidthHeight.y * (0.5f * xwh.h), 0f);
+        return FrameBottomLeftPosition + new Vector3(FrameWidthHeight.x * (xywh.x + 0.5f * xywh.w), FrameWidthHeight.y * (xywh.y + 0.5f * xywh.h), 0f);
     }
 
-    private Vector3 XWH2Scale((float x, float w, float h) xwh)
+    private Vector3 XYWH2Scale((float x, float y, float w, float h) xywh)
     {
-        return new Vector3(FrameWidthHeight.x * xwh.w, FrameWidthHeight.y * xwh.h, 1f); ;
+        return new Vector3(FrameWidthHeight.x * xywh.w, FrameWidthHeight.y * xywh.h, 1f); ;
     }
 
-    private void MatchTransformToXWH(Transform t, (float x, float w, float h) xwh)
+    private void MatchTransformToXYWH(Transform t, (float x, float y, float w, float h) xywh)
     {
-        t.localScale = XWH2Scale(xwh);
-        t.position = XWH2Position(xwh);
+        t.localScale = XYWH2Scale(xywh);
+        t.position = XYWH2Position(xywh);
     }
     #endregion
 }
