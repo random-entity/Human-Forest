bde8de8627409e9ef749e6a1090988570e894d7e
random-entity <yakpanteki@gmail.com>
2021-06-13 23:30:39 +0900

    SVGroup_U_p의 ImageHolder와 UEvalType 버튼이나 키보드로 바꾸기 가능

--- Human Forest/Assets/Main/Scripts/GUI/SVDisplayManager.cs
+++ Human Forest/Assets/Main/Scripts/GUI/SVDisplayManager.cs
@@ -1,10 +1,11 @@
 using System;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class SVDisplayManager : MonoSingleton<SVDisplayManager>
 {
-    private HumanForest hf;
+    private static HumanForest hf;
 
     [SerializeField] private Transform SVDisplayPrefab;
     private float SVDisplayWidth = 1f;
@@ -13,8 +14,12 @@ public class SVDisplayManager : MonoSingleton<SVDisplayManager>
     #region U(p)
     public Dictionary<Person, SVDisplay> SVDisplayGroup_U_p; // RealOrImagePerson p => Quad for displaying U(p)
     public Transform Band_U_p_center;
-    public static EvaluationTypes.Utility UEvalType;
-    public static Person ImageHolder;
+    public static EvaluationTypes.Utility UEvalTypeCurrent;
+    public static EvaluationTypes.Utility UEvalTypeWhenImageHolderRealPerson = EvaluationTypes.Utility.Image_OthersValuesConsiderate;
+    public static EvaluationTypes.Utility UEvalTypeWhenImageHolderGod = EvaluationTypes.Utility.Omniscient; // 변하지 않을 것이다.
+    public static Person ImageHolderCurrent;
+    public static Person ImageHolderWhenGod; // 변하지 않을 것이다.
+    public static Person ImageHolderWhenRealPerson;
 
     private void InitializeSVDisplayGroup_U_p()
     {
@@ -80,63 +85,66 @@ public class SVDisplayManager : MonoSingleton<SVDisplayManager>
 
     public void OnUpdateUEvalTypeOrImageHolder()
     {
-        if (ImageHolder.IsGod)
-        {
-            UEvalType = EvaluationTypes.Utility.Omniscient;
-            SetSVDisplayGroup_U_pActiveByImageHolder(God.god);
-            Debug.Log("setting UEvalType to Omniscient because imageHolder is God.");
-        }
-        else if (ImageHolder.IsReal)
-        {
-            if (UEvalType == EvaluationTypes.Utility.Omniscient)
-            {
-                UEvalType = EvaluationTypes.Utility.Image_OthersValuesConsiderate;
-                Debug.Log("setting UEvalType to Image_OthersValuesConsiderate because imageHolder is a real Person.");
-            }
+        SetSVDisplayGroup_U_pActiveByImageHolder(ImageHolderCurrent);
 
-            // 먼저 Omniscient인지 확인하고 맞으면 바꾸고 그 다음 진행
-
-            if (UEvalType == EvaluationTypes.Utility.Image_OthersValuesConsiderate)
-            {
+        switch (UEvalTypeCurrent)
+        {
+            case EvaluationTypes.Utility.Omniscient:
                 SetSVDisplayGroup_U_pRefToHfPM2SV();
-                SetSVDisplayGroup_U_pActiveByImageHolder(ImageHolder);
-            }
-            else if (UEvalType == EvaluationTypes.Utility.Image_NonOthersValuesConsiderate)
-            {
-                SetSVDisplayGroup_U_pRefToPM2SV(hf.QsStatePsValue[ImageHolder]);
-                SetSVDisplayGroup_U_pActiveByImageHolder(ImageHolder);
-            }
+                break;
 
+            case EvaluationTypes.Utility.Image_OthersValuesConsiderate:
+                SetSVDisplayGroup_U_pRefToHfPM2SV();
+                break;
+
+            case EvaluationTypes.Utility.Image_NonOthersValuesConsiderate:
+                SetSVDisplayGroup_U_pRefToPM2SV(hf.QsStatePsValue[ImageHolderCurrent]);
+                break;
+
+            default:
+                break;
         }
 
-        // switch (UEvalType)
-        // {
-        //     case EvaluationTypes.Utility.Omniscient:
-        //         SetSVDisplayGroup_U_pActiveByImageHolder(God.god);
-        //         break;
-
-        //     case EvaluationTypes.Utility.Image_OthersValuesConsiderate:
-        //         SetSVDisplayGroup_U_pRefToHfPM2SV();
-        //         SetSVDisplayGroup_U_pActiveByImageHolder(ImageHolder);
-        //         break;
-
-        //     case EvaluationTypes.Utility.Image_NonOthersValuesConsiderate:
-        //         SetSVDisplayGroup_U_pRefToPM2SV(hf.QsStatePsValue[ImageHolder]);
-        //         SetSVDisplayGroup_U_pActiveByImageHolder(ImageHolder);
-        //         break;
-        // }
+        EventManager.InvokeOnUpdateSVListRef(); // SVList들에게 무슨 변화가 생기면 꼭 불러줍시다.
     }
 
-    public void UpdateUEvalType(EvaluationTypes.Utility uEvalType)
+    // ImageHolder = ~~~ 나 UEvalTypeCurrent = ~~~ 같이 직접 assign하지 말고 Update뭐시기 메소드를 이용하세요.
+
+    public void UpdateUEvalType(EvaluationTypes.Utility newUEvalType)
     {
-        UEvalType = uEvalType;
+        if (newUEvalType == EvaluationTypes.Utility.Omniscient)
+        {
+            ImageHolderCurrent = ImageHolderWhenGod;
+            UEvalTypeCurrent = newUEvalType;
+        }
+        else
+        {
+            UEvalTypeWhenImageHolderRealPerson = newUEvalType;
+            UEvalTypeCurrent = UEvalTypeWhenImageHolderRealPerson;
+
+            if (ImageHolderCurrent == God.god)
+            {
+                ImageHolderCurrent = ImageHolderWhenRealPerson;
+            }
+        }
+
         OnUpdateUEvalTypeOrImageHolder();
     }
 
-    public void UpdateImageHolder(Person imageHolder)
+    public void UpdateImageHolder(Person newImageHolder)
     {
-        ImageHolder = imageHolder;
-        Debug.Log("imageHolder set to " + imageHolder.gameObject.name);
+        if (newImageHolder.IsGod)
+        {
+            ImageHolderCurrent = ImageHolderWhenGod;
+            UEvalTypeCurrent = UEvalTypeWhenImageHolderGod;
+        }
+        else
+        {
+            ImageHolderWhenRealPerson = newImageHolder;
+            ImageHolderCurrent = ImageHolderWhenRealPerson;
+            UEvalTypeCurrent = UEvalTypeWhenImageHolderRealPerson;
+        }
+
         OnUpdateUEvalTypeOrImageHolder();
     }
     #endregion
@@ -150,7 +158,9 @@ public class SVDisplayManager : MonoSingleton<SVDisplayManager>
     {
         hf = HumanForest.instance;
 
-        if (ImageHolder == null) ImageHolder = hf.RealSociety[0];
+        ImageHolderWhenRealPerson = hf.RealSociety[0];
+        ImageHolderWhenGod = God.god;
+        ImageHolderCurrent = God.god;
 
         InitializeSVDisplayGroup_U_p();
 
@@ -178,5 +188,32 @@ public class SVDisplayManager : MonoSingleton<SVDisplayManager>
     private void OnDisable()
     {
         EventManager.OnGUI_U_p_Click -= UpdateImageHolder;
+    }
+
+
+
+    [SerializeField] Text Text_ImageHolder;
+    [SerializeField] Text Text_UEvalType;
+
+    private int index = 0;
+    private void Update()
+    {
+        Text_ImageHolder.text = "ImageHolder = " + ImageHolderCurrent.gameObject.name;
+        Text_UEvalType.text = "UEvalTypeCurrent = " + UEvalTypeCurrent.ToString();
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            UpdateImageHolder(God.god);
+        }
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            UpdateImageHolder(hf.RealSociety[0]);
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            index++;
+
+            UpdateUEvalType(UEvalTypeCurrent = (EvaluationTypes.Utility)(index % 3));
+        }
     }
 }
--- Human Forest/Assets/Main/Scripts/System/God.cs
+++ Human Forest/Assets/Main/Scripts/System/God.cs
@@ -1,22 +1,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class God : MonoBehaviour
+public class God : MonoSingleton<God>
 {
     private HumanForest hf;
     public static Person god;
 
     public Dictionary<Person, float> ConsiderationEthic_PureUtilitarian;
 
-    [SerializeField] private Person personPrefab;
-    private void Awake()
+    public override void Init()
     {
         hf = HumanForest.instance;
-        
-        god = Instantiate(personPrefab);
+        god = GetComponent<Person>();
+
         god.gameObject.name = "God";
-        god.gameObject.SetActive(false);
         god.IsGod = true;
+
+        god.gameObject.SetActive(false);
 
         ConsiderationEthic_PureUtilitarian = new Dictionary<Person, float>();
         foreach (Person p in hf.RealAndImagesSociety)
